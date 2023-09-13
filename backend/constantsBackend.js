69bc6cdbdc52377628d39091b19f6ceb0cf09d0e
Bram Eldering <bram.eldering@gmail.com>
2023-09-13 17:29:27 +0200

    test husky running lint staged

--- backend/constantsBackend.js
+++ backend/constantsBackend.js
@@ -1,4 +1,5 @@
 export const PUBLIC_URL = 'public';
+test;
 export const UPLOADS_URL = 'uploads';
 export const MAX_IMAGE_FILE_SIZE = 10 * 1000 * 1000; // 10 MB
 export const CLOUDINARY_SAMPLE_IMAGE_URL =
---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+export default {
+  '*.{ts,tsx}': [
+    'npm run lint',
+    "bash -c 'npm run types:check'",
+    'npm run format:check',
+  ],
+};
