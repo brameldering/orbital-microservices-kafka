11e6d926eed063431c341963adafc69f5998c16e
Bram Eldering <bram.eldering@gmail.com>
2023-10-27 13:13:14 +0200

    Fixed header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Link from 'next/link';
 import Container from 'react-bootstrap/Container';
 import { FaUser } from 'react-icons/fa';
 import Router from 'next/router';
@@ -25,45 +26,58 @@ const Header: React.FC<THeaderProps> = ({ currentUser }) => {
     <header>
       <Navbar bg='primary' variant='dark' expand='sm' collapseOnSelect>
         <Container style={{ marginLeft: '5px' }}>
-          <Navbar.Brand href='/' id='LINK_orbitelco_shop'>
-            <LogoSVG />
-            <Navbar.Text style={{ marginLeft: '10px', color: '#6aa0cb' }}>
-              Orbitelco Shop
-            </Navbar.Text>
-          </Navbar.Brand>
-
+          <Link href='/'>
+            <Navbar.Brand id='LINK_orbitelco_shop'>
+              <LogoSVG />
+              <Navbar.Text style={{ marginLeft: '10px', color: '#6aa0cb' }}>
+                Orbitelco Shop
+              </Navbar.Text>
+            </Navbar.Brand>{' '}
+          </Link>
+          {/* <div className='d-flex justify-content-end'> */}
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
+            <Nav className='d-flex justify-content-end'>
               {currentUser?.name ? (
                 <>
                   <NavDropdown
                     title={currentUser.name}
                     id='LINK_header_username'>
-                    <NavDropdown.Item href='/' id='LINK_my_profile'>
-                      My Profile
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href='/' id='LINK_my_orders'>
-                      My Orders
-                    </NavDropdown.Item>
+                    <Link href='/'>
+                      <NavDropdown.Item id='LINK_my_profile'>
+                        My Profile
+                      </NavDropdown.Item>
+                    </Link>
+                    <Link href='/'>
+                      <NavDropdown.Item id='LINK_my_orders'>
+                        My Orders
+                      </NavDropdown.Item>
+                    </Link>
                   </NavDropdown>
                   <Nav.Link id='LINK_header_logout' onClick={logoutHandler}>
-                    Logout
+                    Sign Out
                   </Nav.Link>
                 </>
               ) : (
                 <>
-                  <Nav.Link href='/auth/signin' id='LINK_header_sign_in'>
+                  <Nav.Link
+                    as={Link}
+                    href='/auth/signin'
+                    id='LINK_header_sign_in'>
                     <FaUser /> Sign In
                   </Nav.Link>
-                  <Nav.Link href='/auth/signup' id='LINK_header_sign_up'>
+                  <Nav.Link
+                    as={Link}
+                    href='/auth/signup'
+                    id='LINK_header_sign_up'>
                     <FaUser /> Sign Up
                   </Nav.Link>
                 </>
               )}
+              {errors}
             </Nav>
-            {errors}
           </Navbar.Collapse>
+          {/* </div> */}
         </Container>
       </Navbar>
     </header>
