e7610bc644aaa8646564d1e79ef511b62dec562c
Bram Eldering <bram.eldering@gmail.com>
2023-11-23 13:57:31 +0100

    Intermediate not working code trying to initialize userInfo code from session cookie api

--- client/slices/authSlice.ts
+++ client/slices/authSlice.ts
@@ -1,15 +1,30 @@
-import { createSlice, createAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import {
+  ICurrentUser,
+  IUserInfo,
+  IUser,
+  CURRENT_USER_URL,
+} from '@orbitelco/common';
 
-// import type { IUserInfo } from '../types/authTypes';
-import { IUserInfo, IUser } from '@orbitelco/common';
-
-// const userInfoLocalStorage =
-//   typeof window !== 'undefined' ? localStorage.getItem('userInfo') : null;
-
-// const initialState: IUserInfo = {
-//   userInfo: userInfoLocalStorage ? JSON.parse(userInfoLocalStorage) : null,
-// };
+// Used to fetch user info upon initialization from cookie
+const fetchUserInfo = createAsyncThunk<ICurrentUser, void>(
+  'auth/fetchUserInfo',
+  async () => {
+    try {
+      // Perform API call to retrieve user info from session cookie
+      const response = await fetch(CURRENT_USER_URL);
+      if (response.ok) {
+        const userData = await response.json();
+        return userData as ICurrentUser;
+      }
+      throw new Error('Failed to fetch user info');
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+      throw error;
+    }
+  }
+);
 
 const initialState: IUserInfo = { userInfo: null };
 
@@ -19,28 +34,22 @@ const authSlice = createSlice({
   reducers: {
     setUserState: (state, action: PayloadAction<IUser>) => {
       state.userInfo = action.payload;
-      // if (typeof window !== 'undefined') {
-      //   localStorage.setItem('userInfo', JSON.stringify(action.payload));
-      //   const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000; // 30 days
-      //   localStorage.setItem('expirationTime', expirationTime.toString());
-      // }
     },
     updUserState: (state, action: PayloadAction<IUser>) => {
-      // Update userInfo state and localstorage in case of
-      // MyProfile Update or when an admin user changes it's info using the user admin
+      // Update the userInfo stateonly when the logged in user is the same as user being updated
+      // This is the case of MyProfile Update or when an admin user changes it's info in the User Edit screen
       if (state.userInfo?.id === action.payload.id) {
         state.userInfo = action.payload;
-        // if (typeof window !== 'undefined') {
-        //   localStorage.setItem('userInfo', JSON.stringify(action.payload));
-        // }
       }
     },
     logout: (state) => {
       state.userInfo = null;
-      // if (typeof window !== 'undefined') {
-      //   localStorage.clear();
-      // }
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserInfo.fulfilled, (state, action) => {
+      state.userInfo = action.payload;
+    });
   },
 });
 
@@ -49,5 +58,26 @@ const logout = createAction('auth/logout');
 
 export const { setUserState, updUserState } = authSlice.actions;
 export { logout };
+
+export const initializeUser = () => async (dispatch: any) => {
+  try {
+    const actionResult = await dispatch(fetchUserInfo()); // Dispatch the async action
+
+    // 'actionResult' is the value returned from the dispatched action
+    if (fetchUserInfo.fulfilled.match(actionResult)) {
+      // Check if the dispatched action is fulfilled
+      const userData = actionResult.payload; // Extract the user data from the payload
+
+      // Do something with the user data, e.g., dispatch another action or set it to the state
+      dispatch(setUserState(userData));
+    } else {
+      // Handle other cases like pending, rejected, etc.
+      // For example:
+      console.error('Error fetching user info:', actionResult.error);
+    }
+  } catch (error) {
+    console.error('Error initializing user info:', error);
+  }
+};
 
 export default authSlice.reducer;
