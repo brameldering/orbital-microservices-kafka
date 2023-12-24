ac39d18fbfe9c2933da6251f23f828bb22cc904c
Bram Eldering <bram.eldering@gmail.com>
2023-12-24 13:49:45 +0100

    moved mock for kafkajs to @orbitelco/common/__mocks__

--- orders/src/test/jest.setup.ts
+++ orders/src/test/jest.setup.ts
@@ -6,33 +6,7 @@ import { setupApp } from '../app';
 require('dotenv').config();
 
 // ================== Mock kafka.js ====================
-jest.mock('kafkajs', () => {
-  const MockAdmin = jest.fn().mockImplementation(() => ({
-    connect: jest.fn(),
-    disconnect: jest.fn(),
-    listTopics: jest.fn(),
-    createTopics: jest.fn(),
-  }));
-
-  // Mock Kafka class
-  const MockKafka = jest.fn().mockImplementation(() => ({
-    admin: jest.fn().mockImplementation(() => new MockAdmin()),
-  }));
-
-  // Mock logLevel enum
-  const logLevel = {
-    NOTHING: 0,
-    ERROR: 1,
-    WARN: 2,
-    INFO: 4,
-    DEBUG: 5,
-  };
-
-  return {
-    Kafka: MockKafka,
-    logLevel,
-  };
-});
+import '../../node_modules/@orbitelco/common/build/__mocks__/kafkajs';
 
 // ======================= Mock the kafka-wrapper =========================
 jest.mock(
