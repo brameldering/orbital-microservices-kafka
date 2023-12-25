35195e7270c9972aca5dfe3fa2721d0366a86334
Bram Eldering <bram.eldering@gmail.com>
2023-12-25 08:30:08 +0100

    Fixed cypress test

--- client/cypress/e2e/shopping-and-order-admin.cy.ts
+++ client/cypress/e2e/shopping-and-order-admin.cy.ts
@@ -51,8 +51,11 @@ describe('Initialize', () => {
 describe('Shopping tests', () => {
   beforeEach(() => cy.visit(HOME_PAGE_URL));
   it('E2E_SHOP_OM_1: Search for products and test back and home links', () => {
+    cy.wait(20000);
     // Check that we are on Products page and all 6 products are shown
-    cy.get('h1').invoke('text').should('equal', H1_PRODUCTS);
+    cy.get('h1')
+      .invoke('text')
+      .should('equal', H1_PRODUCTS, { timeout: 40000 });
     cy.get('[id="product_card"]').should('have.length', 6);
     // search for phone and check that 2 products match
     cy.get('[id="search_keyword"]').type(SEARCH_KEYWORD);
