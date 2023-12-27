5c986ec4e592a550b4aed5241d85ee762df5c519
Bram Eldering <bram.eldering@gmail.com>
2023-12-27 11:02:30 +0100

    Removed express dependency in sequences

--- sequences/src/server.ts
+++ sequences/src/server.ts
@@ -1,7 +1,10 @@
+if (!process.env.DEPLOY_ENV || process.env.DEPLOY_ENV !== 'kubernetes') {
+  require('dotenv').config();
+}
 import mongoose from 'mongoose';
-import { app, setupApp } from './app';
 import {
   kafkaWrapper,
+  getKafkaLogLevel,
   Topics,
   Listener,
   ListenerManager,
@@ -56,6 +59,29 @@ class Server {
   start = async () => {
     try {
       console.log(`${method}: starting server`);
+
+      // ======================================================
+      // Check for existence of ENV variables set in depl files (dev/prod) or .env file for test
+      if (!process.env.MONGO_URI) {
+        console.error('Missing ENV variable for MONGO_URI');
+        process.exit(1);
+      }
+      if (!process.env.KAFKA_BROKERS) {
+        console.error('Missing ENV variable for KAFKA_BROKERS');
+        process.exit(1);
+      }
+      if (!process.env.KAFKA_LOG_LEVEL) {
+        console.error('Missing ENV variable for KAFKA_LOG_LEVEL');
+        process.exit(1);
+      }
+      try {
+        // test if KAFKA_LOG_LEVEL is a valid level
+        getKafkaLogLevel(process.env.KAFKA_LOG_LEVEL);
+      } catch (error: any) {
+        console.error('ENV variable for KAFKA_LOG_LEVEL not valid', error);
+      }
+      // ======================================================
+
       // Ensure Kafka connection
       await kafkaWrapper.connect(
         this.KAFKA_CLIENT_ID,
@@ -101,15 +127,6 @@ class Server {
       // Connect to MongoDB
       await mongoose.connect(process.env.MONGO_URI!);
       console.log(`${method}: Connected to MongoDB`);
-
-      // Setup App including caching of ApiAccess after mongoose connection has been established
-      await setupApp();
-
-      // Start listening
-      const port = process.env.PORT || 3000;
-      app.listen(port, () => {
-        console.log(`Listening on port ${port}`);
-      });
     } catch (error: any) {
       console.error(`Error starting sequences server`, error);
     }
@@ -123,23 +140,23 @@ class Server {
       for (const publisher of Object.values(kafkaWrapper.publishers)) {
         await publisher.shutdown();
       }
-    } catch (err) {
-      console.error('Error during shutdown of publishers:', err);
+    } catch (error: any) {
+      console.error('Error during shutdown of publishers:', error);
     }
     try {
       // Disconnect listener
       if (this.listenerManager) {
         await this.listenerManager.disconnect();
       }
-    } catch (err) {
-      console.error('Error during disconnect of listenerManager:', err);
+    } catch (error: any) {
+      console.error('Error during disconnect of listenerManager:', error);
     }
     try {
       // Disconnect Kafka client and admin
       await kafkaWrapper.disconnect();
       console.log('Kafka client disconnected');
-    } catch (err) {
-      console.error('Error during disconnect of kafka client:', err);
+    } catch (error: any) {
+      console.error('Error during disconnect of kafka client:', error);
     }
     try {
       // Disconnect MongoDB connection
@@ -147,8 +164,8 @@ class Server {
       console.log('MongoDB disconnected');
 
       process.exit(0);
-    } catch (err) {
-      console.error('Error during MongoDB disconnect:', err);
+    } catch (error: any) {
+      console.error('Error during MongoDB disconnect:', error);
       process.exit(1);
     }
   };
@@ -160,3 +177,16 @@ server.start();
 // Handle graceful shutdown
 process.on('SIGTERM', server.shutDown);
 process.on('SIGINT', server.shutDown);
+
+process.on('uncaughtException', (err: any) => {
+  console.error('Shutting down due to uncaught exception');
+  console.error(`ERROR: ${err.stack}`);
+  process.exit(1);
+});
+
+// Handle Unhandled Promise rejections
+process.on('unhandledRejection', (err: any) => {
+  console.error('Shutting down the server due to Unhandled Promise rejection');
+  console.error(`ERROR: ${err.stack}`);
+  process.exit(1);
+});
